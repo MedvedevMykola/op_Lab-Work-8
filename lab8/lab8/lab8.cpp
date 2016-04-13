f7fcbb4f76d7c126846aaa787eec2f5700b498d4
test <medved85755@gmail.com>
2016-04-13 19:50:50 +0300

    add function (filegeneratio)

--- lab8/lab8/lab8.cpp
+++ lab8/lab8/lab8.cpp
@@ -1,11 +1,47 @@
-// lab8.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <errno.h>
 
+void filegeneration()
+{
+	char filename[20] = { 0 }, ch;
+	puts("Enter name of file:");
+	gets_s(filename);
+	strcat(filename, ".txt");
+	FILE *f = fopen(filename, "wt+");
+	if (f != NULL)
+		puts("\nfile created");
+	else
+	{
+		perror("can not create file");
+		_getch();
+		exit(EXIT_FAILURE);
+	}
+	puts("Enter some string");
+	do
+	{
+		ch = _getch();
+		if (ch != 26)
+		{
+			if (ch == '\r')
+				ch = '\n';
+			printf("%c", ch);
+			fputc(ch, f);
+		}
+		else
+			break;
+
+	} while (true);
+
+}
 
 int main()
 {
-    return 0;
+	filegeneration();
+	_getch();
+	return 0;
 }
 
