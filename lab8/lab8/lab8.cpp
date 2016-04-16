0708e31032e692eb7d55acfbf802c87c5c0eef05
test <medved85755@gmail.com>
2016-04-16 18:33:13 +0300

    done

--- lab8/lab8/lab8.cpp
+++ lab8/lab8/lab8.cpp
@@ -35,74 +35,87 @@ void filegeneration(char *filename)
 	printf("\n");
 	fclose(f);
 }
-
-void findword(char *filename, char ch)
+void output(char *filename)
 {
-	char l[1] = { 0 }, str[10000] = { 0 }, str1[10000] = { 0 }, d[1] = { 0 };
+	FILE *f = fopen(filename, "rt");
+	if (f != NULL) {
+		while (!feof(f))
+		{
+			char ch;
+			fscanf(f, "%c", &ch);
+			printf("%c", ch);
+		}
+		printf("\n");
+	}
+	fclose(f);
+}
+void findword(char *filename, char *filename2, char ch)
+{
+	char l, str[10000] = { 0 }, str1[10000] = { 0 }, d[1] = { 0 }, *s;
 	d[0] = ch;
 	FILE *f = fopen(filename, "rt");
 	if (f != NULL)
 	{
-		puts("\nfile created");
+		puts("file open");
 		int i = 0;
-		
-		while (l[0] = getc(f) != EOF)
-			str[i++] = l[0];
+
+		while (!feof(f)) {
+			fscanf(f, "%c", &l);
+			str[i++] = l;
+		}
 		str[i] = '\0';
 		puts(str);
 	}
 	else
 	{
-		perror("can not create file");
+		perror("can not open file");
 		_getch();
 		exit(EXIT_FAILURE);
 	}
-	
-	char *delimiter = "\., !?:;";
+
+	char *delimiter = " .,!?";
 	char *ptr;
-	ptr = strtok(str,delimiter);
-	while (ptr != NULL) 
+	ptr = strtok(str, delimiter);
+	while (ptr != NULL)
 	{
-		if (strstr(ptr, d) == ptr) 
+		if (ptr[0]==d[0])
 		{
 			strcat(str1, ptr);
 			strcat(str1, " ");
 		}
 		ptr = strtok(nullptr, delimiter);
 	}
+	puts(str1);
 	fclose(f);
-	char filename2[20] = { 0 };
-	puts("Enter name of file:");
-	gets_s(filename2);
-	strcat(filename2, ".txt");
-	FILE *fl = fopen(filename, "wt");
+
+	FILE *fl = fopen(filename2, "wt");
 	if (fl != NULL)
-		puts("\nfile created");
+		puts("file created");
 	else
 	{
 		perror("can not create file");
 		_getch();
 		exit(EXIT_FAILURE);
 	}
-	for (int i = 0; i < strlen(str1); i++)
-	{
-		printf("%c", str1[i]);
-		putc(str1[i], fl);		
-	}
-	
+	 fputs(str1, fl);
 	fclose(fl);
 }
 
 int main()
 {
-	char filename1[20] = { 0 }, filename2[20] = { 0 }, ch;
+	char filename[20] = { 0 }, filename2[20] = { 0 }, ch;
 	puts("Enter name of file:");
-	gets_s(filename1);
-	strcat(filename1, ".txt");
-	filegeneration(filename1);
+	gets_s(filename);
+	strcat(filename, ".txt");
+	filegeneration(filename);
+	output(filename);
+	puts("Enter name of file:");
+	gets_s(filename2);
+	strcat(filename2, ".txt");
 	puts("Enter symbol:");
-	scanf_s("%c\n",&ch);
-	findword(filename1, ch);
+	scanf("%c", &ch);
+	findword(filename, filename2, ch);
+	output(filename2);
 	_getch();
 	return 0;
 }
