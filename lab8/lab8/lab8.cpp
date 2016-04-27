83ab8b7271b75e8591686558eee0ed207de0bd5f
test <medved85755@gmail.com>
2016-04-27 20:21:59 +0300

    ++

--- lab8/lab8/lab8.cpp
+++ lab8/lab8/lab8.cpp
@@ -1,14 +1,5 @@
 #include "stdafx.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <conio.h>
-#include <errno.h>
-void filegeneration(char *filename);
-void findword(char *filename, char *filename2, char ch);
-void output(char *filename);
-void add(char *filename, char *filename2, char ch);
-void rewrite(char *filename);
+#include "lab.h"
 
 int main()
 {
